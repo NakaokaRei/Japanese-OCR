24e9f32522d0676de621ef4cffb0c757c47ff741
NakaokaRei <reideeplearning@gmail.com>
2022-06-10 23:49:05 +0900

    Initial Commit

---
+++ Japanese-OCR/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Japanese-OCR
+//
+//  Created by NakaokaRei on 2022/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Japanese-OCR/Japanese_OCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japanese_OCRApp.swift
+//  Japanese-OCR
+//
+//  Created by NakaokaRei on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct Japanese_OCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
